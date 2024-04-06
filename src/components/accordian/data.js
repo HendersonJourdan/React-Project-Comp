6cfa215afd4e7393e9af73b5da64957ecaed9b36
HendersonJourdan <81892066+HendersonJourdan@users.noreply.github.com>
2024-04-05 23:28:12 -0400

    Add files via upload

---
+++ src/components/accordian/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id : '1',
+        question: "Don't you know about the bird?",
+        answer:
+            "Everybody knows that the bird is the word"
+    },
+    {
+        id : '2',
+        question: "What is the name of the boat Peter buys at auction?",
+        answer:
+            "The  S.S. More Powerful Than Superman, Batman, Spider-Man, and The Incredible Hulk Put Together."
+    },
+    {
+        id : '3',
+        question: "Why is Peter always fighting the giant chicken?",
+        answer:
+            "The giant chicken gave Peter an expired coupon"
+    },
+    {
+        id : '4',
+        question: "What did Peter change Meg's name to on her birth certificate?",
+        answer:
+            "Megatron"
+    },
+];
+
+export default data;
